4fde259c817b02b4dc7f9946919183787997a3ee
vladharangus <vlad.hara99@gmail.com>
2021-04-06 17:15:15 +0300

    WBT

--- src/test/java/LabAssiAsseProjectV02/IntegrationTest.java
+++ src/test/java/LabAssiAsseProjectV02/IntegrationTest.java
@@ -19,38 +19,28 @@ public class IntegrationTest {
     }
 
     private final StudentValidator studentValidator = new StudentValidator();
-    private NotaValidator notaValidator;
     private final TemaValidator temaValidator = new TemaValidator();
     private String filenameStudent = "fisiere/Studenti1.xml";
     private String filenameTema = "fisiere/Teme.xml";
     private String filenameTema1 = "fisiere/Teme1.xml";
     private String filenameNota1 = "fisiere/Note1.xml";
     private String filenameNota = "fisiere/Note.xml";
-    private StudentXMLRepo studentXMLRepo;
-    private NotaXMLRepo notaXMLRepo;
-    private TemaXMLRepo temaXMLRepo;
-    private Service service;
+    private StudentXMLRepo studentXMLRepo = new StudentXMLRepo(filenameStudent);;
+    private NotaXMLRepo notaXMLRepo = new NotaXMLRepo(filenameNota1);;
+    private TemaXMLRepo temaXMLRepo = new TemaXMLRepo(filenameTema1);;
+    private NotaValidator notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
+    private Service service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
     @Test
-    public void tc_1_addAssignmentValidDeadline() {
+    public void tc_1_addAssignment() {
 
-        studentXMLRepo = new StudentXMLRepo(filenameStudent);
-        temaXMLRepo = new TemaXMLRepo(filenameTema1);
-        notaXMLRepo = new NotaXMLRepo(filenameNota);
-        notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
-        service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
 
         Tema t = service.addTema(new Tema("2", "d1", 14, 1));
-        assert(t.getDeadline() == 5);
+        assert(t.getDeadline() == 14);
         //service.deleteTema("2");
     }
     @Test
-    public void tc_1_addStudentValidName() {
+    public void tc_1_addStudent() {
 
-        studentXMLRepo = new StudentXMLRepo(filenameStudent);
-        temaXMLRepo = new TemaXMLRepo(filenameTema);
-        notaXMLRepo = new NotaXMLRepo(filenameNota);
-        notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
-        service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
 
         Student s = service.addStudent("1", "name", 934, "mail");
         assert(s.getNume().equals("name"));
@@ -59,13 +49,8 @@ public class IntegrationTest {
     @Test
     public void tc_1_addGrade() {
 
-        studentXMLRepo = new StudentXMLRepo(filenameStudent);
-        temaXMLRepo = new TemaXMLRepo(filenameTema);
-        notaXMLRepo = new NotaXMLRepo(filenameNota1);
-        notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
-        service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
 
-        Nota g = new Nota("1", "1", "1", 10.0, LocalDate.now());
+        Nota g = new Nota("1", "1", "2", 10.0, LocalDate.now());
         double finalGrade = service.addNota(g, "feedback");
         assert(finalGrade == 10.0);
     }
@@ -73,8 +58,8 @@ public class IntegrationTest {
     @Test
     public void tc_1_BigBangTest() {
 
-        tc_1_addAssignmentValidDeadline();
-        tc_1_addStudentValidName();
+        tc_1_addAssignment();
+        tc_1_addStudent();
         tc_1_addGrade();
 
     }
